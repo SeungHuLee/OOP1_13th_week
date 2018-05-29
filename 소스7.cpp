bf6fe2653d29e361c939781702e7e9515576709b
SeungHuLee <gadlsh12081208@gmail.com>
2018-05-29 16:01:58 +0900

    add descriptions

--- 소스7.cpp
+++ 소스7.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<array>
 #include<algorithm>
-#include<iterator>
+#include<iterator> //begin, end 함수
 using namespace std;
 int main() {
 	// class 형식의 array 선언
@@ -10,7 +10,7 @@ int main() {
 
 	//built in 형식의 array 선언
 	int arr[5] = { 5,8,10,9,4 };
-	//sort(arr, arr+5);
+	//sort(arr, arr+5); arr 시작 주소, arr+5는 자동으로 주소값으로 변환해서 마지막(5*4)
 	sort(begin(arr), end(arr));
 	for (int item : arr) {
 		cout << item << " ";
