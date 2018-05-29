dd642418c104ecbc409a082005b10570abb468f6
SeungHuLee <gadlsh12081208@gmail.com>
2018-05-29 16:11:42 +0900

    add descriptions

--- 소스8.cpp
+++ 소스8.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //두 함수는 동일하다
 //기본적으로 pass-by-reference이기 때문에
 //파라미터로 넘길때 const를 명시해준다
-void printElements1(const int values[], const size_t num);
+void printElements1(const int values[], const size_t num); //출력만 할 것이기 때문에 array argument앞에 const를 붙인다. (array를 받을때는 pass-by-ref로 받기 때문.)
 void printElements2(const int* values, const size_t num);
 int main() {
 	int arr[5] = { 1,5,3,8,5 };
