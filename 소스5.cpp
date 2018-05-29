c88488f3faf94eecf7c4cf30c48fc45b77349faa
SeungHuLee <gadlsh12081208@gmail.com>
2018-05-29 15:49:22 +0900

    add descriptions

--- 소스5.cpp
+++ 소스5.cpp
@@ -8,6 +8,6 @@ int main() {
 	cout << "The address of var is " << &var << endl << endl;
 
 	cout << "The value of ptr is " << ptr << endl;
-	cout << "The value of *ptr is " << *ptr << endl;
+	cout << "The value of *ptr is " << *ptr << endl; // 주소를 반환하는건 &, 주소에 접근하는건 *
 
 }
